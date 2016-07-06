9749499f8a1146abec56084cf35c4523c6e4a55f
simonh <simon@slimtech-it.co.uk>
2016-07-06 08:34:54 +0100

    Added LogMessage and LogMessageException to Contracts. To be used as Dto for log messages over the web

---
+++ Code/EnergyTrading.Contracts/Logging/LogMessage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace EnergyTrading.Contracts.Logging
+{
+    [Serializable]
+    [DataContract]
+    public class LogMessage
+    {
+        [DataMember]
+        public string CreatingType { get; set; }
+
+        [DataMember]
+        public string Level { get; set; }
+
+        [DataMember]
+        public string Message { get; set; }
+
+        /// <summary>
+        /// When contract is used objects in the params must be serializable using the chosen method (e.g. .NetXml, Json, custom serialization logic)
+        /// </summary>
+        [DataMember]
+        public object[] Params { get; set; } 
+
+        /// <summary>
+        /// using a LogMessageException here keeps information while ensuring that the object remains serializable (as many exception types are not serializable despite the guidelines)
+        /// use LogMessageException.FromException(Exception exception) to obtain a LogMessageException
+        /// </summary>
+        [DataMember]
+        public LogMessageException Exception { get; set; }
+    }
+}
---
+++ Code/EnergyTrading.Contracts/Logging/LogMessageException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace EnergyTrading.Contracts.Logging
+{
+    [Serializable]
+    public class LogMessageException : Exception
+    {
+        protected LogMessageException(string message, LogMessageException innerException) : base(message, innerException)
+        {
+        }
+
+        protected LogMessageException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public new string StackTrace { get; set; }
+
+        public static LogMessageException FromException(Exception exception)
+        {
+            if (exception == null)
+            {
+                return null;
+            }
+
+            return new LogMessageException(exception.Message, FromException(exception.InnerException))
+            {
+                Source = exception.Source,
+                HResult = exception.HResult,
+                StackTrace = exception.StackTrace
+            };
+        }
+    }
+}
