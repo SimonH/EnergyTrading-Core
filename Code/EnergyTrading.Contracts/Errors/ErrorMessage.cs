a0bacf2f862dc05d6e7e368572637192a6e09336
simonh <simon@slimtech-it.co.uk>
2016-07-07 14:22:19 +0100

    Error Message (for Client / Server transfer of Errors)

---
+++ Code/EnergyTrading.Contracts/Errors/ErrorMessage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnergyTrading.Contracts.Errors
+{
+    public class ErrorMessage
+    {
+        /// <summary>
+        /// UTC time when the error occurred
+        /// </summary>
+        public DateTime TimeStamp { get; set; }
+
+        /// <summary>
+        /// The broad category of the error (e.g. Business / Technical)
+        /// </summary>
+        public string Category { get; set; }
+
+        /// <summary>
+        /// Unique code identifying the exact error (normally namespaced to prevent clashes)
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// The severity of the error
+        /// </summary>
+        public string Severity { get; set; }
+
+        /// <summary>
+        /// text message describing the detail of the error
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Opaque data related to the error (e.g. serialized payloads, source data) normally interpretted and used at the point of handling the error
+        /// </summary>
+        public IEnumerable<ReferenceData> ReferenceDatas { get; set; }
+
+        /// <summary>
+        /// Additional properties as string:key / string:value pairs
+        /// </summary>
+        public IDictionary<string, string> AdditionalProperties { get; set; }
+    }
+}
---
+++ Code/EnergyTrading.Contracts/Errors/ReferenceData.cs
@@ -0,0 +1,8 @@
+﻿namespace EnergyTrading.Contracts.Errors
+{
+    public class ReferenceData
+    {
+        public string Key { get; set; }
+        public string Data { get; set; }
+    }
+}
