77c843667267e6f88a0b81f070d4d5d0bc1497ee
SimonH <simon@slimtech-it.co.uk>
2016-07-27 14:49:19 +0100

    more generic factory for wrapping

--- Code/EnergyTrading.Core/Logging/FilteredLoggerFactory.cs
+++ Code/EnergyTrading.Core/Logging/FilteredLoggerFactory.cs
@@ -2,52 +2,12 @@
 
 namespace EnergyTrading.Logging
 {
-    public class FilteredLoggerFactory : ILoggerFactory
+    public class FilteredLoggerFactory : WrappingLoggerFactory
     {
         private readonly ILoggerFactory wrappedFactory;
         private readonly LogFilterLevel filterLevel;
-        public FilteredLoggerFactory(ILoggerFactory factoryToWrap, LogFilterLevel level)
+        public FilteredLoggerFactory(ILoggerFactory factoryToWrap, LogFilterLevel level) : base(factoryToWrap, logger => new FilteredLogger(logger, level))
         {
-            if (factoryToWrap == null)
-            {
-                throw new ArgumentNullException(nameof(factoryToWrap));
-            }
-            wrappedFactory = factoryToWrap;
-            filterLevel = level;
-        }
-
-        private ILogger Wrap(ILogger logger)
-        {
-            if (logger == null)
-            {
-                return null;
-            }
-            return new FilteredLogger(logger, filterLevel);
-        }
-
-        public ILogger GetLogger(string name)
-        {
-            return Wrap(wrappedFactory.GetLogger(name));
-        }
-
-        public ILogger GetLogger<T>()
-        {
-            return Wrap(wrappedFactory.GetLogger<T>());
-        }
-
-        public ILogger GetLogger(Type type)
-        {
-            return Wrap(wrappedFactory.GetLogger(type));
-        }
-
-        public void Initialize()
-        {
-            wrappedFactory.Initialize();
-        }
-
-        public void Shutdown()
-        {
-            wrappedFactory.Shutdown();
         }
     }
 }
---
+++ Code/EnergyTrading.Core/Logging/WrappingLoggerFactory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace EnergyTrading.Logging
+{
+    public class WrappingLoggerFactory : ILoggerFactory
+    {
+        private readonly ILoggerFactory wrappedFactory;
+        private readonly Func<ILogger, ILogger> wrapLoggerFunc;
+        public WrappingLoggerFactory(ILoggerFactory wrappedFactory, Func<ILogger, ILogger> wrapLoggerFunc)
+        {
+            if (wrapLoggerFunc == null)
+            {
+                throw new ArgumentNullException(nameof(wrapLoggerFunc));
+            }
+            if (wrappedFactory == null)
+            {
+                throw new ArgumentNullException(nameof(wrappedFactory));
+            }
+            this.wrappedFactory = wrappedFactory;
+            this.wrapLoggerFunc = wrapLoggerFunc;
+        }
+
+        private ILogger Wrap(ILogger logger)
+        {
+            return logger == null ? null : wrapLoggerFunc(logger);
+        }
+
+        public ILogger GetLogger(string name)
+        {
+            return Wrap(wrappedFactory.GetLogger(name));
+        }
+
+        public ILogger GetLogger<T>()
+        {
+            return Wrap(wrappedFactory.GetLogger<T>());
+        }
+
+        public ILogger GetLogger(Type type)
+        {
+            return Wrap(wrappedFactory.GetLogger(type));
+        }
+
+        public void Initialize()
+        {
+            wrappedFactory.Initialize();
+        }
+
+        public void Shutdown()
+        {
+            wrappedFactory.Shutdown();
+        }
+    }
+}
