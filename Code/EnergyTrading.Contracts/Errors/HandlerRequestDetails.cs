5cd3f5487e41fe44fc09696183a9f9b86245d228
SimonH <simon@slimtech-it.co.uk>
2016-08-09 15:38:38 +0100

    Added HandlerRequestDetails to Contract

---
+++ Code/EnergyTrading.Contracts/Errors/HandlerRequestDetails.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EnergyTrading.Contracts.Errors
+{
+    public class HandlerRequestDetails
+    {
+        public string Name { get; set; }
+        public DateTime Since { get; set; }
+
+        public string[] ValidErrorCodes { get; set; }
+    }
+}
