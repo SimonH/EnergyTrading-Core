cb6db30ac917e579bd57a56c6e5d8bb310123131
SimonH <simon@slimtech-it.co.uk>
2016-08-18 13:58:05 +0100

    reference data now has information about data serialization

--- Code/EnergyTrading.Contracts/Errors/ReferenceData.cs
+++ Code/EnergyTrading.Contracts/Errors/ReferenceData.cs
@@ -4,5 +4,7 @@
     {
         public string Key { get; set; }
         public string Data { get; set; }
+        public string SerializationMethod { get; set; }
+        public string TypeName { get; set; }
     }
 }
