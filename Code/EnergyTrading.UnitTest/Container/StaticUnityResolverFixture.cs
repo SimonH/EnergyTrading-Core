620d2814b6e24783af4e6cbffafae0c501632a21
SimonH <simon@slimtech-it.co.uk>
2016-11-29 13:01:41 +0000

    Added StaticUnityResolverBase useful when you need to inject with a default constructor (e.g. Web Service controllers)

---
+++ Code/EnergyTrading.UnitTest/Container/StaticUnityResolverFixture.cs
@@ -0,0 +1,26 @@
+﻿using EnergyTrading.Configuration;
+using EnergyTrading.Container;
+using EnergyTrading.Registrars;
+using Microsoft.Practices.Unity;
+using NUnit.Framework;
+
+namespace EnergyTrading.UnitTest.Container
+{
+    [TestFixture]
+    public class StaticUnityResolverFixture
+    {
+        public class ConfigManagerUnityResolver : StaticUnityResolverBase<ConfigManagerUnityResolver>
+        {
+            protected override void RegisterItems(IUnityContainer container)
+            {
+                new ConfigurationManagerRegistrar().Register(container);
+            }
+        }
+
+        [Test]
+        public void CorrectlyResolvesRegisteredItems()
+        {
+            Assert.That(ConfigManagerUnityResolver.Resolve<IConfigurationManager>(), Is.Not.Null.And.TypeOf<AppConfigConfigurationManager>());
+        }
+    }
+}
---
+++ Code/EnergyTrading.Unity/Container/StaticUnityResolverBase.cs
@@ -0,0 +1,26 @@
+﻿using EnergyTrading.Container.Unity;
+using Microsoft.Practices.Unity;
+
+namespace EnergyTrading.Container
+{
+    public abstract class StaticUnityResolverBase<TDerivedResolver> where TDerivedResolver : StaticUnityResolverBase<TDerivedResolver>, new()
+    {
+        private static IUnityContainer _container;
+        private static IUnityContainer Container => _container ?? (_container = InitialiseContainer());
+
+        private static IUnityContainer InitialiseContainer()
+        {
+            var ret = new UnityContainer();
+            ret.StandardConfiguration();
+            new TDerivedResolver().RegisterItems(ret);
+            return ret;
+        }
+
+        public static T Resolve<T>(string name = null)
+        {
+            return string.IsNullOrEmpty(name) ? Container.Resolve<T>() : Container.Resolve<T>(name);
+        }
+
+        protected abstract void RegisterItems(IUnityContainer container);
+    }
+}
