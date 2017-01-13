259b73864af258d067e369ebd46a102f33c33397
SimonH <simon@slimtech-it.co.uk>
2017-01-13 17:10:53 +0000

    Added standard function for converting sql server timestamp to an unsigned number

---
+++ Code/EnergyTrading.Core/Data/DataConversionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EnergyTrading.Data
+{
+    public static class DataConversionExtensions
+    {
+        public static ulong FromRowVersion(this byte[] rowversion)
+        {
+            if (rowversion.Length != 8)
+            {
+                throw new ArgumentOutOfRangeException(nameof(rowversion), "row versions are expected to be 8 byte arrays");
+            }
+            var copy = new byte[8];
+            Array.Copy(rowversion, copy, 8);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(copy);
+            }
+            return BitConverter.ToUInt64(copy, 0);
+        }
+    }
+}
---
+++ Code/EnergyTrading.UnitTest/Data/DataConversionExtensionsFixture.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using EnergyTrading.Data;
+using NUnit.Framework;
+
+namespace EnergyTrading.UnitTest.Data
+{
+    [TestFixture]
+    public class DataConversionExtensionsFixture
+    {
+        public static IEnumerable FromRowVersionSource
+        {
+            get
+            {
+                yield return new TestCaseData(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }, 0UL).SetName("0");
+                yield return new TestCaseData(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01 }, 1UL).SetName("1");
+                yield return new TestCaseData(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01 }, 257UL).SetName("257");
+                yield return new TestCaseData(new byte[] { 0x9E, 0x00, 0x00, 0x56, 0x00, 0x00, 0x78, 0x00 }, 0x9E00005600007800).SetName("generalbytearray");
+            }
+        }
+
+        [Test]
+        public void InvalidArray()
+        {
+            Assert.That(() => new byte[10].FromRowVersion(), Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        [TestCaseSource(nameof(FromRowVersionSource))]
+        public void FromRowVersion(byte[] source, ulong expected)
+        {
+            Assert.That(source.FromRowVersion(), Is.EqualTo(expected));
+        }
+    }
+}
